01a249302213c3a02cad0303654c9eb8b3dde92c
Eloy Coto <eloy.coto@acalustra.com>
2021-10-06 17:22:15 +0200

    Small refactor
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- internal/heartbeat/heartbeat.go
+++ internal/heartbeat/heartbeat.go
@@ -16,82 +16,25 @@ import (
 	pb "github.com/redhatinsights/yggdrasil/protocol"
 )
 
-type Heartbeat struct {
-	ticker           *time.Ticker
-	dispatcherClient pb.DispatcherClient
-	configManager    *cfg.Manager
-	workloadManager  *workld.WorkloadManager
-	dataMonitor      *datatransfer.Monitor
-	hardware         *hw.Hardware
+type HeartbeatData struct {
+	configManager   *cfg.Manager
+	workloadManager *workld.WorkloadManager
+	dataMonitor     *datatransfer.Monitor
+	hardware        *hw.Hardware
 }
 
-func NewHeartbeatService(dispatcherClient pb.DispatcherClient, configManager *cfg.Manager,
-	workloadManager *workld.WorkloadManager, hardware *hw.Hardware, dataMonitor *datatransfer.Monitor) *Heartbeat {
-	return &Heartbeat{
-		dispatcherClient: dispatcherClient,
-		configManager:    configManager,
-		workloadManager:  workloadManager,
-		hardware:         hardware,
-		dataMonitor:      dataMonitor,
+func NewHeartbeatData(configManager *cfg.Manager,
+	workloadManager *workld.WorkloadManager, hardware *hw.Hardware, dataMonitor *datatransfer.Monitor) *HeartbeatData {
+
+	return &HeartbeatData{
+		configManager:   configManager,
+		workloadManager: workloadManager,
+		hardware:        hardware,
+		dataMonitor:     dataMonitor,
 	}
 }
 
-func (s *Heartbeat) Start() {
-	s.initTicker(s.getInterval(s.configManager.GetDeviceConfiguration()))
-}
-
-func (s *Heartbeat) HasStarted() bool {
-	return s.ticker != nil
-}
-
-func (s *Heartbeat) getInterval(config models.DeviceConfiguration) int64 {
-	var interval int64 = 60
-
-	if config.Heartbeat != nil {
-		interval = config.Heartbeat.PeriodSeconds
-	}
-	if interval <= 0 {
-		interval = 60
-	}
-	return interval
-}
-
-func (s *Heartbeat) PushInformation(ctx context.Context) error {
-
-	// Create a data message to send back to the dispatcher.
-	heartbeatInfo := s.getHeartbeatInfo()
-
-	content, err := json.Marshal(heartbeatInfo)
-	if err != nil {
-		return err
-	}
-
-	data := &pb.Data{
-		MessageId: uuid.New().String(),
-		Content:   content,
-		Directive: "heartbeat",
-	}
-
-	_, err = s.dispatcherClient.Send(ctx, data)
-	return err
-}
-
-func (s *Heartbeat) initTicker(periodSeconds int64) {
-	ticker := time.NewTicker(time.Second * time.Duration(periodSeconds))
-	s.ticker = ticker
-	go func() {
-		for range ticker.C {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-			defer cancel()
-			err := s.PushInformation(ctx)
-			if err != nil {
-				log.Errorf("Heartbeat interval cannot send the data, err: %s", err)
-			}
-		}
-	}()
-}
-
-func (s *Heartbeat) getHeartbeatInfo() models.Heartbeat {
+func (s *HeartbeatData) RetrieveInfo() models.Heartbeat {
 	var workloadStatuses []*models.WorkloadStatus
 	workloads, err := s.workloadManager.ListWorkloads()
 	for _, info := range workloads {
@@ -127,6 +70,33 @@ func (s *Heartbeat) getHeartbeatInfo() models.Heartbeat {
 	return heartbeatInfo
 }
 
+type Heartbeat struct {
+	ticker           *time.Ticker
+	dispatcherClient pb.DispatcherClient
+	data             *HeartbeatData
+}
+
+func NewHeartbeatService(dispatcherClient pb.DispatcherClient, configManager *cfg.Manager,
+	workloadManager *workld.WorkloadManager, hardware *hw.Hardware, dataMonitor *datatransfer.Monitor) *Heartbeat {
+	return &Heartbeat{
+		ticker:           nil,
+		dispatcherClient: dispatcherClient,
+		data: &HeartbeatData{
+			configManager:   configManager,
+			workloadManager: workloadManager,
+			hardware:        hardware,
+			dataMonitor:     dataMonitor},
+	}
+}
+
+func (s *Heartbeat) Start() {
+	s.initTicker(s.getInterval(s.data.configManager.GetDeviceConfiguration()))
+}
+
+func (s *Heartbeat) HasStarted() bool {
+	return s.ticker != nil
+}
+
 func (s *Heartbeat) Update(config models.DeviceConfigurationMessage) error {
 	periodSeconds := s.getInterval(*config.Configuration)
 	log.Infof("Reconfiguring ticker with interval: %v", periodSeconds)
@@ -135,4 +105,51 @@ func (s *Heartbeat) Update(config models.DeviceConfigurationMessage) error {
 	}
 	s.initTicker(periodSeconds)
 	return nil
+}
+
+func (s *Heartbeat) getInterval(config models.DeviceConfiguration) int64 {
+	var interval int64 = 60
+
+	if config.Heartbeat != nil {
+		interval = config.Heartbeat.PeriodSeconds
+	}
+	if interval <= 0 {
+		interval = 60
+	}
+	return interval
+}
+
+func (s *Heartbeat) pushInformation(ctx context.Context) error {
+
+	// Create a data message to send back to the dispatcher.
+	heartbeatInfo := s.data.RetrieveInfo()
+
+	content, err := json.Marshal(heartbeatInfo)
+	if err != nil {
+		return err
+	}
+
+	data := &pb.Data{
+		MessageId: uuid.New().String(),
+		Content:   content,
+		Directive: "heartbeat",
+	}
+
+	_, err = s.dispatcherClient.Send(ctx, data)
+	return err
+}
+
+func (s *Heartbeat) initTicker(periodSeconds int64) {
+	ticker := time.NewTicker(time.Second * time.Duration(periodSeconds))
+	s.ticker = ticker
+	go func() {
+		for range ticker.C {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			err := s.pushInformation(ctx)
+			if err != nil {
+				log.Errorf("Heartbeat interval cannot send the data, err: %s", err)
+			}
+		}
+	}()
 }
