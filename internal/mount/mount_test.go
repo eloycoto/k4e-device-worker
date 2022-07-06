9ac75b29bf9d792433d60d347f60d0d35f121edc
Eloy Coto <eloy.coto@acalustra.com>
2022-07-06 12:55:33 +0200

    wip
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- internal/mount/mount_test.go
+++ internal/mount/mount_test.go
@@ -2,6 +2,7 @@ package mount_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"syscall"
 
@@ -20,36 +21,54 @@ var _ = Describe("Mount", func() {
 		otherBlockDevice string
 		charDevicePath   string
 		devFolder        string
+		tmpFolder        string
+		err              error
 	)
+
+	// Creating block-device
+	// sudo mknod block_device b 89 1
+	// creating char-device
+	// sudo mknod block_device c 89 1
 
 	Context("Mount block device", func() {
 		BeforeEach(func() {
-			// SkipIfRootless("need root permission")
+
+			tmpFolder, err = ioutil.TempDir("/tmp/", "mountfile")
+			Expect(err).NotTo(HaveOccurred())
+
 			depMock = &util.MockIDependencies{}
 			devFolder = "/dev/foodev"
-			Expect(os.MkdirAll(devFolder, os.ModePerm)).To(BeNil())
+
+			Expect(os.RemoveAll(devFolder)).To(BeNil())
+			Expect(os.MkdirAll(devFolder, os.ModePerm)).NotTo(HaveOccurred())
 
 			blockDevicePath = fmt.Sprintf("%s/blockdevice", devFolder)
-			Expect(unix.Mknod(blockDevicePath, syscall.S_IFBLK|uint32(os.FileMode(0660)), int(unix.Mkdev(7, 0)))).To(BeNil())
+			Expect(unix.Mknod(blockDevicePath, syscall.S_IFBLK|syscall.S_IRUSR|syscall.S_IWUSR|syscall.S_IWGRP, int(unix.Mkdev(7, 0)))).
+				NotTo(HaveOccurred())
 
 			otherBlockDevice = fmt.Sprintf("%s/otherblockdevice", devFolder)
-			Expect(unix.Mknod(otherBlockDevice, syscall.S_IFBLK|uint32(os.FileMode(0660)), int(unix.Mkdev(7, 0)))).To(BeNil())
+			Expect(unix.Mknod(otherBlockDevice, syscall.S_IFBLK|uint32(os.FileMode(0660)), int(unix.Mkdev(7, 0)))).
+				NotTo(HaveOccurred())
 
 			charDevicePath = fmt.Sprintf("%s/chardevice", devFolder)
-			Expect(unix.Mknod(charDevicePath, syscall.S_IFCHR|uint32(os.FileMode(0660)), int(unix.Mkdev(3, 1)))).To(BeNil())
+			Expect(unix.Mknod(charDevicePath, syscall.S_IFCHR|uint32(os.FileMode(0660)), int(unix.Mkdev(3, 1)))).
+				NotTo(HaveOccurred())
 		})
+
 		AfterEach(func() {
 			depMock.AssertExpectations(GinkgoT())
 
-			Expect(os.RemoveAll(devFolder)).To(BeNil())
+			// Expect(os.RemoveAll(devFolder)).To(BeNil())
+			Expect(os.RemoveAll(tmpFolder)).To(BeNil())
+
 		})
 
-		It("mount block device without error", func() {
+		FIt("mount block device without error", func() {
 			// given
 			dep := util.NewDependencies("/")
 			mount := models.Mount{
-				Device:    blockDevicePath,
-				Directory: "/mnt",
+				Device:    "/tmp/block_device",
+				Directory: tmpFolder,
 				Type:      "ext4",
 			}
 
@@ -63,17 +82,29 @@ var _ = Describe("Mount", func() {
 			Expect(err).To(BeNil())
 
 			// when
-			Expect(mountManager.Update(configuration)).To(BeNil())
-			defer func() {
-				Expect(unix.Unmount(mount.Device, 0)).To(BeNil())
-			}()
+			Expect(mountManager.Update(configuration)).NotTo(HaveOccurred())
 
 			// then
-			_, mounts, err := mm.GetMounts(dep)
-			Expect(err).To(BeNil())
+			a, mounts, err := mm.GetMounts(dep)
+			Expect(err).NotTo(HaveOccurred())
+
+			debug := func() {
+				fmt.Printf("A =%+v\n", a)
+				fmt.Printf("Mounts =%+v\n", mounts)
+
+				for key := range mounts {
+					fmt.Println("Key--->", key)
+				}
+				fmt.Println(mount.Directory)
+
+				fmt.Printf("Err =%+v\n", err)
+			}
+			debug()
 
 			_, found := mounts[mount.Directory]
 			Expect(found).To(BeTrue())
+
+			Expect(unix.Unmount(mount.Device, 0)).NotTo(HaveOccurred())
 		})
 
 		It("Don't try to mount char devices", func() {
